0746255cb0057de73b571a128cf8e8695481bd64
achrysaetos <achrysaetos@yahoo.com>
2024-07-23 16:59:10 -0500

    finish styling with tailwind

--- bun-frontend/components/TodoApp.tsx
+++ bun-frontend/components/TodoApp.tsx
@@ -1,4 +1,7 @@
 import { useState } from "react";
+import { Input } from "./ui/input";
+import { Checkbox } from "./ui/checkbox";
+import { Button } from "./ui/button";
 
 interface TodoItem {
     id: number;
@@ -23,27 +26,25 @@ export default function TodoApp() {
   };
 
   return (
-    <div>
-        <h1>Todo List</h1>
-        <input type="text" value={todo} onChange={(e) => setTodo(e.target.value)} />
-        <button onClick={() => addTodo(todo)}>Add</button>
-        <ul>
+    <div className="flex flex-col items-center w-1/3 mx-auto mt-10">
+        <h1 className="text-2xl font-bold mb-4">Todo</h1>
+        <span className="flex items-center gap-2 mb-4 w-full">
+            <Input value={todo} onChange={(e) => setTodo(e.target.value)} />
+            <Button onClick={() => {addTodo(todo); setTodo("");}} disabled={!todo}>Add</Button>
+        </span>
+        <ul className="w-full px-1">
             {todos.map((item) => (
-                <li key={item.id}>
-                    <input
-                        type="checkbox"
-                        checked={item.completed}
-                        onChange={() => toggleTodo(item.id)}
-                    />
-                    <span style={{ textDecoration: item.completed ? 'line-through' : 'none' }}>
-                        {item.title}
-                    </span>
+                <li key={item.id} className="flex items-center gap-2 justify-between">
+                    <div className="flex items-center gap-2">
+                        <Checkbox checked={item.completed} onCheckedChange={() => toggleTodo(item.id)} />
+                        <label className={item.completed ? "line-through" : ""}>
+                            {item.title}
+                        </label>
+                    </div>
                     <button onClick={() => deleteTodo(item.id)}>Delete</button>
                 </li>
             ))}
         </ul>
     </div>
   );
-
-
 };
